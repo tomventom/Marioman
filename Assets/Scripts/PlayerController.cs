4b8c67005941d5304e633699c1d9b4fe99f00e7d
unknown <tomventom@gmail.com>
2017-10-23 17:08:40 +0300

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10;
+    public bool facingRight = true;
+    public float jumpSpeed = 10;
+
+    private float moveX;
+    private Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            Jump();
+        }
+    }
+
+    void FixedUpdate () {
+        PlayerMove();
+	}
+
+    void PlayerMove() {
+        moveX = Input.GetAxis("Horizontal");
+        if (moveX > 0f && !facingRight) {
+            facingRight = true;
+            GetComponent<SpriteRenderer>().flipX = false;
+        } else if (moveX < 0f && facingRight) {
+            facingRight = false;
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+    }
+
+    void Jump() {
+        rb.AddForce(new Vector2(0f, jumpSpeed), ForceMode2D.Impulse);
+    }
+
+}
